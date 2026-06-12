4adfa42d359a141444258a369a9aefae1ee97c56
Ronaldo <skri_into@hotmail.com>
2026-06-12 18:57:54 -0500

    Crear estructura inicial de la clase Venta

---
+++ venta.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Venta {
+    private final String id;
+    private final LocalDate fecha;
+    private double total;
+}
